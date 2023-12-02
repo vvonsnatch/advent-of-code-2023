c8ba6ac7a151725242aa22e2d9e654e6d6ed9f7b
Veronica Suprun <veronica.suprun@wealthsimple.com>
2023-12-02 02:17:28 -0500

    right answer

--- src/main/kotlin/Day1pt2.kt
+++ src/main/kotlin/Day1pt2.kt
@@ -1,57 +1,38 @@
 import java.io.File
 
 fun main() {
+    print(File("./input/day1/data")
+        .readLines()
+        .mapIndexed() { i, line ->
+            var ints = emptyArray<Int>()
 
-        File("./input/day1/data")
-            .readLines()
-            .mapIndexed() { i, line ->
-                var pos = 0
-                var ints = emptyArray<Int>()
-
-                while (pos < line.length) {
-                    if (line[pos].isDigit()) {
-                        ints += line[pos].digitToInt()
-                        pos++
-                    } else {
-                        if (pos + 2 < line.length && line.substring(pos..pos + 2) == "one") {
-                            ints += 1
-                            pos += 3
-                        } else if (pos + 2 < line.length && line.substring(pos..pos + 2) == "two") {
-                            ints += 2
-                            pos += 3
-                        } else if (pos + 4 < line.length && line.substring(pos..pos + 4) == "three") {
-                            ints += 3
-                            pos += 4
-                        } else if (pos + 3 < line.length && line.substring(pos..pos + 3) == "four") {
-                            ints += 4
-                            pos += 3
-                        } else if (pos + 3 < line.length && line.substring(pos..pos + 3) == "five") {
-                            ints += 5
-                            pos += 3
-                        } else if (pos + 2 < line.length && line.substring(pos..pos + 2) == "six") {
-                            ints += 6
-                            pos += 2
-                        } else if (pos + 4 < line.length && line.substring(pos..pos + 4) == "seven") {
-                            ints += 7
-                            pos += 4
-                        } else if (pos + 4 < line.length && line.substring(pos..pos + 4) == "eight") {
-                            ints += 8
-                            pos += 4
-                        } else if (pos + 3 < line.length && line.substring(pos..pos + 3) == "nine") {
-                            ints += 9
-                            pos += 3
-                        } else {
-                            pos++
-                        }
+            for (pos in line.indices) {
+                if (line[pos].isDigit()) {
+                    ints += line[pos].digitToInt()
+                } else {
+                    if (pos + 2 < line.length && line.substring(pos..pos + 2) == "one") {
+                        ints += 1
+                    } else if (pos + 2 < line.length && line.substring(pos..pos + 2) == "two") {
+                        ints += 2
+                    } else if (pos + 4 < line.length && line.substring(pos..pos + 4) == "three") {
+                        ints += 3
+                    } else if (pos + 3 < line.length && line.substring(pos..pos + 3) == "four") {
+                        ints += 4
+                    } else if (pos + 3 < line.length && line.substring(pos..pos + 3) == "five") {
+                        ints += 5
+                    } else if (pos + 2 < line.length && line.substring(pos..pos + 2) == "six") {
+                        ints += 6
+                    } else if (pos + 4 < line.length && line.substring(pos..pos + 4) == "seven") {
+                        ints += 7
+                    } else if (pos + 4 < line.length && line.substring(pos..pos + 4) == "eight") {
+                        ints += 8
+                    } else if (pos + 3 < line.length && line.substring(pos..pos + 3) == "nine") {
+                        ints += 9
                     }
                 }
-
-                val sum = if (ints.size == 0) 0 else "${ints[0]}${ints[ints.size - 1]}".toInt()
-
-                println("${i+1} ${line} ${sum}")
-
-                sum
             }
-//            .reduce { acc, num -> acc + num },
 
+            if (ints.size == 0) 0 else "${ints[0]}${ints[ints.size - 1]}".toInt()
+        }
+        .reduce { acc, num -> acc + num },)
 }
